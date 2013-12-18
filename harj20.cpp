1f3a424b79b8a2b9f7c486ba4dbb438b5c154aab
kuikkanoora <kuikkanoora@gmail.com>
2013-12-19 01:39:42 +0200

    Harjoitus 20
    Noora Kuikka - H4719

---
+++ harj20.cpp
@@ -0,0 +1,48 @@
+/*************************************************************************************************************************************************************
+*Tehtävä: Harjoitus20
+*Tekijä: Noora Kuikka
+*PVM: 10.11.2013
+*Kuvaus:
+*Kuten harjoitus 19, mutta toteuta kayttaen taulukkoa ja osoitinta
+*tiedon käsittelyyn
+*Versio 1.0 H4719 10.11.2013
+**************************************************************************************************************************************************************/
+
+#include <iostream>
+using namespace std;
+
+const int SPEED_ARRAY = 5;
+
+int main()
+{
+	int speed[SPEED_ARRAY] = {0,0,0,0,0};
+	int sum;
+	int med;
+	int i = 0;
+	int input;
+	int *sp;
+	sp = &speed[i];
+
+
+	cout << "\nThis program acts as a car's speed meter.\nEnter a negative integer to quit the program.\n" << endl;
+
+	do
+	{
+		cout << "Enter your speed: ";
+		cin >> speed[i];
+		input = *sp;
+		sum = speed[0]+speed[1]+speed[2]+speed[3]+speed[4];
+		med = sum/SPEED_ARRAY;
+		if (input >=0)
+		{
+			cout << " ---> " << med << "km/h" << endl;
+		}
+
+		i++;
+		if (i>4)
+		{
+			i=0;
+		}
+	}
+	while(input >= 0);
+}
